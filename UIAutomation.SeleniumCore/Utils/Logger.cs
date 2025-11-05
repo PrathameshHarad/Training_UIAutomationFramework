859d2ac499a6433dc50d28ed9448cd722749ede1
Prathamesh Baban Harad <prathamesh_harad@epam.com>
2025-11-05 15:16:50 +0530

    logger

--- UIAutomation.SeleniumCore/Utils/Logger.cs
+++ UIAutomation.SeleniumCore/Utils/Logger.cs
@@ -1,55 +1,27 @@
 ﻿using log4net;
 using log4net.Appender;
+using log4net.Config;
 using log4net.Layout;
 using log4net.Repository.Hierarchy;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace UIAutomation.SeleniumCore.Utils
 {
     public static class Logger
     {
-        private static readonly ILog log;
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         static Logger()
         {
-            var hierarchy = (Hierarchy)LogManager.GetRepository();
-
-            var patternLayout = new PatternLayout
-            {
-                ConversionPattern = "[%date] [%level] %logger - %message%newline"
-            };
-            patternLayout.ActivateOptions();
-
-            var consoleAppender = new ConsoleAppender
-            {
-                Layout = patternLayout
-            };
-            consoleAppender.ActivateOptions();
-
-            var fileAppender = new RollingFileAppender
-            {
-                AppendToFile = true,
-                File = "Logs/TestAutomation.log",
-                Layout = patternLayout,
-                MaxSizeRollBackups = 5,
-                MaximumFileSize = "5MB",
-                RollingStyle = RollingFileAppender.RollingMode.Size,
-                StaticLogFileName = true
-            };
-            fileAppender.ActivateOptions();
-
-            hierarchy.Root.AddAppender(consoleAppender);
-            hierarchy.Root.AddAppender(fileAppender);
-
-            hierarchy.Root.Level = log4net.Core.Level.Debug;
-            hierarchy.Configured = true;
-
-            log = LogManager.GetLogger(typeof(Logger));
+            var logRepository = LogManager.GetRepository(Assembly.GetExecutingAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
         }
 
         public static void Info(string message) => log.Info(message);
-        public static void Debug(string message) => log.Debug(message);
         public static void Warn(string message) => log.Warn(message);
         public static void Error(string message, Exception ex = null) => log.Error(message, ex);
+        public static void Debug(string message) => log.Debug(message);
     }
 }
